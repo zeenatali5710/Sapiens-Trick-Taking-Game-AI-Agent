aad9e5ff3a8f7c67457c606fd2cd24298c2751d5
shreyadate05 <36097825+shreyadate05@users.noreply.github.com>
2019-11-20 19:44:53 -0500

    Add files via upload

--- sapiens.py
+++ sapiens.py
@@ -1,6 +1,6 @@
 class Player(object):
 
-    def __init__(self):
+	def __init__(self):
 		self.sAgentName = "Sapiens"
 		self.hand       = []
 		self.cards      = []
@@ -8,59 +8,67 @@ class Player(object):
 		self.pastTricks = []
 		self.scoreMap   = { }	# dictionary to maintain mapping of player name and his/her score 
 
-    def get_name(self):
-        """
-        Returns a string of the agent's name
-        """
-        return self.sAgentName
+	def get_name(self):
+		"""
+		Returns a string of the agent's name
+		"""
+		return self.sAgentName
 
-    def get_hand(self):
-        """
-        Returns a list of two character strings reprsenting cards in the agent's hand
-        """
-        return self.hand
+	def get_hand(self):
+		"""
+		Returns a list of two character strings reprsenting cards in the agent's hand
+		"""
+		return self.hand
 
-    def new_hand(self, names):
-        """
-        Takes a list of names of all agents in the game in clockwise playing order
-        and returns nothing. This method is also responsible for clearing any data
-        necessary for your agent to start a new round.
-        """
-        self.players = names
-        self.scoreMap = { key: 0 for key in self.players } 
+	def new_hand(self, names):
+		"""
+		Takes a list of names of all agents in the game in clockwise playing order
+		and returns nothing. This method is also responsible for clearing any data
+		necessary for your agent to start a new round.
+		"""
+		self.players = names
+		self.scoreMap = { key: 0 for key in self.players } 
 
-    def add_cards_to_hand(self, cards):
-        """
-        Takes a list of two character strings representing cards as an argument
-        and returns nothing.
-        This list can be any length.
-        """
-        self.hand = cards
+	def add_cards_to_hand(self, cards):
+		"""
+		Takes a list of two character strings representing cards as an argument
+		and returns nothing.
+		This list can be any length.
+		"""
+		self.hand = cards
 
-    def play_card(self, lead, trick):
-        """
-        Takes a a string of the name of the player who lead the trick and
-        a list of cards in the trick so far as arguments.
+	def play_card(self, lead, trick):
+		"""
+		Takes a a string of the name of the player who lead the trick and
+		a list of cards in the trick so far as arguments.
 
-        Returns a two character string from the agents hand of the card to be played
-        into the trick.
-        """
+		Returns a two character string from the agents hand of the card to be played
+		into the trick.
+		"""
 
-        #actual logic 
-        pass
+		#actual logic 
+		pass
 
-    def collect_trick(self, lead, winner, trick):
-        """
-        Takes three arguements. Lead is the name of the player who led the trick.
-        Winner is the name of the player who won the trick. And trick is a four card
-        list of the trick that was played. Should return nothing.
-        """
-        trickHistoryData = (lead, winner, trick)
-        self.pastTricks.append(trickHistoryData)
-        self.scoreMap[winner] += 1
+	def collect_trick(self, lead, winner, trick):
+		"""
+		Takes three arguements. Lead is the name of the player who led the trick.
+		Winner is the name of the player who won the trick. And trick is a four card
+		list of the trick that was played. Should return nothing.
+		"""
+		trickHistoryData = (lead, winner, trick)
+		self.pastTricks.append(trickHistoryData)
+		self.scoreMap[winner] += 1
 
-    def score(self):
-        """
-        Calculates and returns the score for the game being played.
-        """
-        return self.scoreMap[self.sAgentName]
+	def score(self):
+		"""
+		Calculates and returns the score for the game being played.
+		"""
+		return self.scoreMap[self.sAgentName]
+
+
+def main():
+	p = Player();
+	print(p.get_name())
+
+if __name__ == '__main__':
+	main()
